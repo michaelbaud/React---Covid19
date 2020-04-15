d5ede1a476bcfeaf74a1059aa07f52e2aed8ea06
michaelbaud <michael.baudoin.7@gmail.com>
2020-04-15 06:17:25 +0200

    -couleur listIcon -modif footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,8 +9,9 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div><MdEmail className="footerLogo" size={25} /> <a href="mailto:michael.baudoin.7@gmail.com">Me contacter</a></div>
-            <div><FaGithub className="footerLogo" size={25} /> <a href="https://github.com/michaelbaud" target="_blank">Mon github</a></div>
-            <div><FaCode className="footerLogo" size={25} /> <a href="https://github.com/michaelbaud/covid19" target="_blank">Site open source by Mikadev via Github</a></div>
+            <div><FaGithub className="footerLogo" size={25} /> <a href="https://github.com/michaelbaud" target="_blank">Mon Github</a></div>
+            <div><FaCode className="footerLogo" size={25} /> <a href="https://github.com/michaelbaud/covid19" target="_blank">Code source du site</a></div>
+            <div>Made by Mikadev</div>
         </footer>
     )
 }
--- src/components/NotifUpdated.js
+++ src/components/NotifUpdated.js
@@ -5,7 +5,7 @@ const NotifUpdated = () => {
     return (
         <div>
             <div className="notification is-warning is-inline-block">
-                <p>Les données pour la France sont mise à jour aux alentours de 20h00</p>
+                <p>Les données pour la France sont mise à jour aux alentours de 20H00</p>
             </div>
         </div>
 
