2d1050d52068a01b7e810a0fa32d149fd468dbbc
michaelbaud <michael.baudoin.7@gmail.com>
2020-04-15 04:39:37 +0200

    add code source github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,14 @@ import React from 'react'
 // React icon
 import { MdEmail } from "react-icons/md"
 import { FaGithub } from "react-icons/fa"
+import { FaCode } from "react-icons/fa"
 
 const Footer = () => {
     return (
         <footer className="footer">
             <div><MdEmail className="footerLogo" size={25} /> <a href="mailto:michael.baudoin.7@gmail.com">Me contacter</a></div>
             <div><FaGithub className="footerLogo" size={25} /> <a href="https://github.com/michaelbaud" target="_blank">Mon github</a></div>
-            <div>Copyright &copy; 2020 Mikadev / Tous droits réservés</div>
+            <div><FaCode className="footerLogo" size={25} /> <a href="https://github.com/michaelbaud/covid19" target="_blank">Site open source by Mikadev via Github</a></div>
         </footer>
     )
 }
