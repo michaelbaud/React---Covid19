f3849619e9fdb845c864a923c26f58b0e88ee880
michaelbaud <michael.baudoin.7@gmail.com>
2020-04-22 04:01:04 +0200

    -modif footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,6 @@ import { FaCode } from "react-icons/fa"
 const Footer = () => {
     return (
         <footer className="footer">
-            <div><MdEmail className="footerLogo" size={25} /> <a href="mailto:michael.baudoin.7@gmail.com">Me contacter</a></div>
-            <div><FaGithub className="footerLogo" size={25} /> <a href="https://github.com/michaelbaud" target="_blank" rel="noopener noreferrer">Mon Github</a></div>
             <div><FaCode className="footerLogo" size={25} /> <a href="https://github.com/michaelbaud/covid19" target="_blank" rel="noopener noreferrer">Code source du site</a></div>
             <div>Made by Michael Baudoin</div>
         </footer>
